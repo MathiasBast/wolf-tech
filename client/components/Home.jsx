c37fb68adbdf22889977e237a1f166847ce3bbb0
Mathias Bast <mathiasbast123@gmail.com>
2020-06-05 12:06:53 +1200

    adds style sheet

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <div>
+      <h1>WOLF TECH</h1>
+      <p>-CCTV  -ALARMS  -ACCESS CONTROL</p>
+    </div>
   )
 }
 
