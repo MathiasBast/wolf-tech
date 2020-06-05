492701012bfd3b78ed1916f49fd5fa2efae015ec
Mathias Bast <mathiasbast123@gmail.com>
2020-06-05 17:37:01 +1200

    adds home text

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -2,10 +2,27 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='main-container' >
-      <h1 className='main-headder' >WOLF TECH</h1>
-      <h2 className='main-subheadding' >-CCTV   -ALARMS   -ACCESS CONTROL</h2>
-    </div>
+    <>
+      <div className='main-container' >
+        <h1 className='main-headder' >WOLF TECH</h1>
+        <h2 className='main-subheadding' >-CCTV   -ALARMS   -ACCESS CONTROL</h2>
+      </div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet nisl purus in mollis nunc sed.
+          Urna cursus eget nunc scelerisque viverra. Faucibus scelerisque eleifend donec pretium. Elementum eu facilisis sed odio. Tortor at risus viverra adipiscing at in tellus
+          integer feugiat. Orci phasellus egestas tellus rutrum tellus. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Amet porttitor eget dolor morbi
+          non arcu. Purus in massa tempor nec feugiat nisl pretium. Id nibh tortor id aliquet lectus proin nibh nisl condimentum. Nibh praesent tristique magna sit amet purus
+          gravida quis blandit. Massa tincidunt dui ut ornare lectus sit amet est.
+        </p>
+        <p>
+          Est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat. Id consectetur purus ut faucibus pulvinar elementum integer enim neque. Vel risus commodo
+          viverra maecenas. Proin fermentum leo vel orci porta non. Sagittis vitae et leo duis ut diam quam. Montes nascetur ridiculus mus mauris vitae ultricies leo. Diam quis
+          enim lobortis scelerisque fermentum dui faucibus. Enim ut tellus elementum sagittis. At augue eget arcu dictum varius duis at. Fames ac turpis egestas integer eget
+          aliquet. Nam libero justo laoreet sit amet cursus sit amet.
+        </p>
+      </div>
+    </>
   )
 }
 
