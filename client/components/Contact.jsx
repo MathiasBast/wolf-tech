6865fcb03ecd6fc6b3c58a9a2bff2ee69820d896
Mathias Bast <mathiasbast123@gmail.com>
2020-06-05 17:29:57 +1200

    adds styling to about

--- client/components/Contact.jsx
+++ client/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
         <h1 className='main-headder' >WOLF TECH</h1>
         <h2 className='main-subheadding' >Contact</h2>
       </div>
-      <div>
+      <div className='contact-body' >
         <a href = "mailto: wbast@xnet.co.nz">Email: wbast@xnet.co.nz</a>
         <p>Phone Number: 021 660 688</p>
       </div>
