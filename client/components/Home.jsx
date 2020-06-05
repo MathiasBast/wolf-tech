d307b5ac2db287803b5acd0351717cb08046ba27
Mathias Bast <mathiasbast123@gmail.com>
2020-06-05 12:15:05 +1200

    adds stuuf

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <h1>WOLF TECH</h1>
-      <p>-CCTV  -ALARMS  -ACCESS CONTROL</p>
+    <div className='main-container' >
+      <h1 className='main-headder' >WOLF TECH</h1>
+      <h2 className='main-subheadding' >-CCTV   -ALARMS   -ACCESS CONTROL</h2>
     </div>
   )
 }
