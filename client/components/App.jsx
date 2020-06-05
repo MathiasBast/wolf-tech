094b824f61e141eeed4e8ecfcb6adea5d67cc96b
Mathias Bast <mathiasbast123@gmail.com>
2020-06-05 12:04:23 +1200

    started headder/ navbar

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+import Home from './Home'
+import Headder from './Headder'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <>
+    <Route path='/' component={Headder} />
+      <Switch>
+        <Route exact path='/' component={Home} />
+      </Switch>
+    </>
   )
 }
 
---
+++ client/components/Headder.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <div>
+        <button>
+          Home
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default Footer
