421c40571412dd1f6b76613acc7fe4e84aa6085a
Mathias Bast <mathiasbast123@gmail.com>
2020-06-05 13:04:50 +1200

    adds abouit stuff

--- client/components/About.jsx
+++ client/components/About.jsx
@@ -8,7 +8,46 @@ const About = () => {
         <h2 className='main-subheadding' >About Me</h2>
       </div>
       <div className='about-text' >
-        <p>Helloooo</p>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit scelerisque
+          in dictum non consectetur a erat nam. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat in. Egestas integer eget aliquet nibh
+          praesent tristique. Tincidunt praesent semper feugiat nibh sed pulvinar. Id venenatis a condimentum vitae sapien. Eget felis eget nunc lobortis
+          mattis aliquam faucibus purus. Lorem dolor sed viverra ipsum nunc aliquet bibendum. Lacinia quis vel eros donec ac odio tempor. Auctor augue
+          mauris augue neque gravida in fermentum et sollicitudin. Nulla pharetra diam sit amet nisl suscipit adipiscing. Cras pulvinar mattis nunc sed
+          blandit libero volutpat sed. Lorem ipsum dolor sit amet consectetur adipiscing elit. Libero enim sed faucibus turpis. Nulla pharetra diam sit
+          amet nisl suscipit. Id diam vel quam elementum pulvinar etiam. Nulla malesuada pellentesque elit eget gravida cum.
+        </p>
+        <p>
+          Viverra nam libero justo laoreet. Eu sem integer vitae justo eget magna fermentum iaculis. Lobortis mattis aliquam faucibus purus in. Fermentum
+          leo vel orci porta non pulvinar. Varius vel pharetra vel turpis nunc eget lorem dolor sed. Orci phasellus egestas tellus rutrum tellus
+          pellentesque. Eget magna fermentum iaculis eu non. Sed euismod nisi porta lorem mollis aliquam. Duis ultricies lacus sed turpis tincidunt id
+          aliquet risus. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac.
+        </p>
+        <p>
+          Praesent tristique magna sit amet purus gravida. Vitae sapien pellentesque habitant morbi tristique senectus. Magna sit amet purus gravida quis
+          blandit turpis. Mauris in aliquam sem fringilla ut morbi. Ut venenatis tellus in metus vulputate eu scelerisque. Enim blandit volutpat maecenas
+          volutpat blandit aliquam etiam. Urna id volutpat lacus laoreet non curabitur gravida arcu. Dictumst quisque sagittis purus sit amet volutpat
+          consequat mauris. Nunc scelerisque viverra mauris in aliquam sem. Lectus arcu bibendum at varius vel pharetra. Sed egestas egestas fringilla
+          phasellus. Cras pulvinar mattis nunc sed blandit libero. Odio euismod lacinia at quis risus sed vulputate.
+        </p>
+        <p>
+          Et malesuada fames ac turpis egestas integer eget aliquet. Ut lectus arcu bibendum at varius vel. Venenatis a condimentum vitae sapien
+          pellentesque. Vestibulum mattis ullamcorper velit sed ullamcorper. Velit laoreet id donec ultrices tincidunt. Risus at ultrices mi tempus
+          imperdiet nulla. Leo duis ut diam quam nulla porttitor massa. Praesent tristique magna sit amet purus gravida quis blandit turpis. Felis
+          bibendum ut tristique et. Integer vitae justo eget magna fermentum iaculis eu non diam. Non consectetur a erat nam. Tempor nec feugiat nisl
+          pretium fusce id velit. Eu mi bibendum neque egestas congue quisque egestas diam in. Faucibus nisl tincidunt eget nullam non nisi est sit amet.
+          Cras tincidunt lobortis feugiat vivamus. Commodo nulla facilisi nullam vehicula ipsum. Nisl nisi scelerisque eu ultrices vitae auctor.
+          Sollicitudin aliquam ultrices sagittis orci a. Tellus id interdum velit laoreet id.
+        </p>
+        <p>
+          In nibh mauris cursus mattis molestie a. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Pharetra magna ac placerat vestibulum.
+          Faucibus a pellentesque sit amet porttitor eget. Sed velit dignissim sodales ut eu. Felis eget velit aliquet sagittis id consectetur. Pretium
+          fusce id velit ut tortor pretium viverra. Elementum tempus egestas sed sed. Potenti nullam ac tortor vitae purus. Lorem sed risus ultricies
+          tristique nulla aliquet enim tortor. Vitae turpis massa sed elementum tempus egestas sed. Bibendum neque egestas congue quisque egestas diam.
+          Amet consectetur adipiscing elit pellentesque habitant. Dapibus ultrices in iaculis nunc. Id ornare arcu odio ut. Lacus vestibulum sed arcu non.
+          Id neque aliquam vestibulum morbi blandit cursus. Gravida rutrum quisque non tellus orci ac auctor augue mauris.
+        </p>
       </div>
     </>
   )
---
+++ client/components/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <h1>COnstact</h1>
+  )
+}
+
+export default Contact
